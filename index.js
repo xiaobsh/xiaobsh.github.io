91d695563f753bcb6b4434f0024182d55405ccc4
xiaobsh <88483240+xiaobsh@users.noreply.github.com>
2021-12-05 15:53:39 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function jumppage(){
+  var text = document.getElementById('code').value;
+  window.open("https://xiaobsh.github.io/pages/" + text);
+}
+
+//document.getElementById('jump').onclick = jumppage;
